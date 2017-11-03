a010bd9b653fb52bab07fd459cd2fe4a3b6e1847
Evert Smits <evert@appedemic.nl>
2017-11-03 12:04:57 +0100

    working api calls

--- miraBeauWebApp/src/api/group/people-api.ts
+++ miraBeauWebApp/src/api/group/people-api.ts
@@ -4,29 +4,33 @@ export class PeopleApi extends baseApi {
 
   servicePath = "persongroups/mirabeaugroup_mobile/persons";
 
-  getPeople() {
-    this.servicePath += "?";
-    return this._get(this.servicePath);
-  }
+   servicePath = "persongroups/mirabeaugroup_mobile/persons";
+ 
+   getPeople() {
+    console.warn("GETPEOPLE")
+    return this._get(this.servicePath + "?");
+   }
+ 
+   deletePerson(id: string) {
+     console.warn("DELETEPERSON")
+     return this._delete(this.servicePath + "/" + id);
+   }
+ 
+   updatePerson(id: string, formData: FormData) {
+     console.warn("UPDATEPERSON")
+     return this._postParams(this.servicePath + "/" + id + "?");
+   }
 
-  deletePerson(id: string) {
-    this.servicePath += "/" + id;
-    return this._delete(this.servicePath);
-  }
 
-  updatePerson(id: string, formData: FormData) {
-    this.servicePath += "/" + id + "?";
-    return this._postParams(this.servicePath);
-  }
+   getPerson(id: string) {
+     console.warn("GETPERSON")
+     return this._get(this.servicePath + "/" + id);
+   }
+ 
+   addPerson(formData: FormData) {
+     console.warn("ADDPERSON")
+     return this._postParams(this.servicePath + "?", formData);
+   }
 
-  getPerson(id: string) {
-    this.servicePath += "/" + id;
-    return this._get(this.servicePath);
-  }
-
-  addPerson(formData: FormData) {
-    this.servicePath += "?";
-    return this._postParams(this.servicePath, formData);
-  }
 
 }

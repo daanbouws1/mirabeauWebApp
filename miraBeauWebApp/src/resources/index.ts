430f1959c79f0b4a091dcb110b8d859174938359
Daan Bouws <daanbouws1@gmail.com>
2017-11-14 21:28:08 +0100

    added new image + fixed bug that made app crash sometimes

--- miraBeauWebApp/src/resources/index.ts
+++ miraBeauWebApp/src/resources/index.ts
@@ -3,8 +3,7 @@ import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    "./value-converters/filter",
-    "./value-converters/filter-params",
+    "./value-converters/filter"
   ]);
 
 }
